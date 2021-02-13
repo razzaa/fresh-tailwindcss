04a49f8f3fc2dc9f2464ffb798e9643344991a27
codewithali <ali-raza@ashlarglobal.com>
2021-02-13 17:20:34 +0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @include('layouts.app')
+</head>
+<body>
+    <div class="w-screen h-screen bg-gray-900">
+        
+    </div>
+</body>
+</html>
